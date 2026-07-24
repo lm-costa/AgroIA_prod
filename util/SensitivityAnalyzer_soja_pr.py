da4eb554630206564fbc9c8f2d2b013ab7102c33
lm-costa <luism.costa00@gmail.com>
2026-07-24 20:55:20 -0300

    funcoes de analise de sensibilidade

--- util/SensitivityAnalyzer_soja_pr.py
+++ util/SensitivityAnalyzer_soja_pr.py
@@ -19,11 +19,44 @@ from pcse.input import CABOFileReader, YAMLAgroManagementReader
 from pcse.input import YAMLCropDataProvider
 from pcse.input.sitedataproviders import WOFOST72SiteDataProvider
 
-from SensitivityAnalyzer import MorrisScreeningAnalyzer
+from SensitivityAnalyzer import MorrisScreeningAnalyzer, NetCDFDataLoader
 from utils_soja_pr import map_info_soja_pr, update_agro_management_file_soja_pr
 
 # Importações do SALib usadas apenas dentro do método sobrescrito
 from SALib.analyze import morris as morris_analyzer_salib
+
+
+class SoyNetCDFDataLoader(NetCDFDataLoader):
+    """
+    NetCDFDataLoader para os arquivos point_id_*.nc gerados pelo pipeline
+    soja/PR (3.Climatic_data_Soja_PR.ipynb), que ja armazenam as variaveis no
+    formato do WOFOST (IRRAD, TMIN, TMAX, T2M, VAP, RAIN, WIND).
+
+    O NetCDFDataLoader original (pipeline de milho) espera os nomes brutos da
+    NASA POWER (ALLSKY_SFC_SW_DWN, PRECTOTCORR, T2M_MAX/MIN, RH2M, WS2M) e
+    faz a conversao de unidades dentro de convert_to_wofost_format -- aqui a
+    conversao ja foi feita na etapa 3, entao so precisamos remontar o
+    DataFrame a partir das variaveis existentes, sem reconverter nada.
+    """
+
+    @staticmethod
+    def convert_to_wofost_format(ds):
+        n = len(ds['date'])
+        yield_values = ds['yield'].values if 'yield' in ds.data_vars else np.full(n, np.nan)
+        dyield_values = ds['dyield'].values if 'dyield' in ds.data_vars else np.full(n, np.nan)
+
+        return pd.DataFrame({
+            'date': ds['date'].values,
+            'IRRAD': ds['IRRAD'].values,
+            'TMIN': ds['TMIN'].values,
+            'TMAX': ds['TMAX'].values,
+            'T2M': ds['T2M'].values,
+            'VAP': ds['VAP'].values,
+            'RAIN': ds['RAIN'].values,
+            'WIND': ds['WIND'].values,
+            'yield': yield_values,
+            'dyield': dyield_values,
+        })
 
 
 class SoyMorrisScreeningAnalyzerPR(MorrisScreeningAnalyzer):
